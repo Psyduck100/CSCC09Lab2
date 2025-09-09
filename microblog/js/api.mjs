f4c7d8a411e10c7170ba7a8bb4be27a2bd8e6681
Rogers Dai <roger.dai.78@gmail.com>
2025-09-09 19:16:07 -0400

    Update api.mjs

--- microblog/js/api.mjs
+++ microblog/js/api.mjs
@@ -1,62 +1,53 @@
-
 // retrieve all messages
-export function getMessages(){
-    let posts = {};
+export function getMessages() {
+  let posts = {};
 
-
-    if (localStorage.getItem('postsMade') == null){
-    localStorage.setItem('postsMade', '0');
-    } 
-    else{
+  if (localStorage.getItem("posts") == null) {
+    localStorage.setItem("postsMade", "0");
+    localStorage.setItem("posts", JSON.stringify(posts));
+  } else {
     //parse posts
-    posts = JSON.parse(localStorage.getItem('posts'));
-    }
+    posts = JSON.parse(localStorage.getItem("posts"));
+  }
 
-    return posts
+  return posts;
 }
 
 // add a message
-export function addMessage(author, content){
+export function addMessage(author, content) {
+  let posts = JSON.parse(localStorage.getItem("posts")) || {};
 
-    let posts = JSON.parse(localStorage.getItem('posts'));
+  let messageID = parseInt(localStorage.getItem("postsMade")) + 1;
+  let post_info = {
+    username: author,
+    content: content,
+    upvotes: 0,
+    downvotes: 0,
+    messageID: messageID,
+  };
+  posts[messageID] = post_info;
 
-    let messageID = parseInt(localStorage.getItem('postsMade')) + 1
-    let post_info = {'username': author, 
-      'content': content,
-      'upvotes': 0,
-      'downvotes': 0,
-      'messageID': messageID
-    }
-    posts[messageID] = post_info;
+  localStorage.setItem("postsMade", String(messageID));
+  localStorage.setItem("posts", JSON.stringify(posts));
 
-    localStorage.setItem('postsMade', String(messageID));
-    localStorage.setItem('posts', JSON.stringify(posts));
-
-    return messageID
+  return messageID;
 }
 
 // delete a message given its messageId
-export function deleteMessage(messageId){
-
-    let posts = JSON.parse(localStorage.getItem('posts'));
-    delete posts[messageId];
-    localStorage.setItem('posts', JSON.stringify(posts));
-
+export function deleteMessage(messageId) {
+  let posts = JSON.parse(localStorage.getItem("posts"));
+  delete posts[messageId];
+  localStorage.setItem("posts", JSON.stringify(posts));
 }
 
-export function downvoteMessage(messageID){
-
-    let posts = JSON.parse(localStorage.getItem('posts'));
-    posts[messageID]['downvotes'] += 1;
-    localStorage.setItem('posts', JSON.stringify(posts));
-
+export function downvoteMessage(messageID) {
+  let posts = JSON.parse(localStorage.getItem("posts"));
+  posts[messageID]["downvotes"] += 1;
+  localStorage.setItem("posts", JSON.stringify(posts));
 }
 
-
-export function upvoteMessage(messageID){
-
-    let posts = JSON.parse(localStorage.getItem('posts'));
-    posts[messageID]['upvotes'] += 1;
-    localStorage.setItem('posts', JSON.stringify(posts));
-
+export function upvoteMessage(messageID) {
+  let posts = JSON.parse(localStorage.getItem("posts"));
+  posts[messageID]["upvotes"] += 1;
+  localStorage.setItem("posts", JSON.stringify(posts));
 }
